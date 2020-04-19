1e9af53bec4cb422f1b12b7d9b3f69cedba25592
ZeNiXxX <zenixxx.havoc@gmail.com>
2020-04-19 22:54:06 +0200

    soong: allow curl + ping

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -117,6 +117,8 @@ var Configuration = map[string]PathConfig{
 	"zip":      Allowed,
 	"zipinfo":  Allowed,
 	"svn":  Allowed,
+	"curl":  Allowed,
+	"ping":  Allowed,
 
 	// Host toolchain is removed. In-tree toolchain should be used instead.
 	// GCC also can't find cc1 with this implementation.
